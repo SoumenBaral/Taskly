11b9073cf19caaa0278d25671cf375e23e2a02a5
SoumenBaral <shumen.sb@gmail.com>
2023-11-16 17:34:44 +0600

    upate the task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,22 +3,26 @@ const AddTask = ({tasks,setTasklist,task,setTask}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         if(task.Id){
-
+            const newDate = new Date();
+            const updateTaskList = tasks.map(newTask =>(
+                newTask.Id === task.Id?{Id:task.Id,name:task.name ,Time:`${newDate.toLocaleTimeString()} ${newDate.toLocaleDateString()}`}: newTask
+            ))
+            setTasklist(updateTaskList)
         }
         else{
-        const date = new Date();
-        // console.log(e.target.task.value ,date.getTime());
-        const newTask = {
-            Id:date.getTime(),
-            name : e.target.task.value, 
-            Time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
-        }
-        const text = e.target.task.value;
-        const length = text.length
-        if(length>0){
-            setTasklist([...tasks,newTask])
-             e.target.task.value = ""
-        }
+            const date = new Date();
+            // console.log(e.target.task.value ,date.getTime());
+            const newTask = {
+                Id:date.getTime(),
+                name : e.target.task.value, 
+                Time:`${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+                }   
+            const text = e.target.task.value;
+            const length = text.length
+            if(length>0){
+                setTasklist([...tasks,newTask])
+                e.target.task.value = ""
+                }
         }
         
 
@@ -28,7 +32,7 @@ const AddTask = ({tasks,setTasklist,task,setTask}) => {
     return (
        <section className="addTask">
             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Add Task" autoComplete="off" name="task" maxLength={25} uncontrolled="true" value={task.name}  />
+                <input type="text" placeholder="Add Task" autoComplete="off" name="task" maxLength={25} uncontrolled="true" value={task.name} onChange={e=>setTask({...task,name:e.target.value})} />
                 <button type="Submit">Add</button>
             </form>
        </section>
